be692d24fbb5920818d58730d9b4917a424ab7b8
Varun <varunkumar.pinnam@gmail.com>
2026-01-28 13:00:59 +0530

    updated varaiables templates

--- varaibles.sh
+++ varaibles.sh
@@ -3,10 +3,10 @@
 #P1=$1
 #P2=$2
 
-echo "$1:: Hello $2, How are you?"
-echo "$2:: Hi $1, I am fine thanks for asking. How are you doing?"
-echo "$1:: doing fine. What are you learning $2?"
-echo "$2:: I am learning Shell. What about you?"
+echo "$P1:: Hello $P2, How are you?"
+echo "$P2:: Hi $P1, I am fine thanks for asking. How are you doing?"
+echo "$P1:: doing fine. What are you learning $P2?"
+echo "$P2:: I am learning Shell. What about you?"
 
 ##### OR
 
@@ -17,3 +17,13 @@ echo "$2:: I am learning Shell. What about you?"
 # echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
 # echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
 # echo "$PERSON2:: I am learning Shell. What about you?"
+
+#### OR
+
+# #P1=$1
+# #P2=$2
+
+# echo "$1:: Hello $2, How are you?"
+# echo "$2:: Hi $1, I am fine thanks for asking. How are you doing?"
+# echo "$1:: doing fine. What are you learning $2?"
+# echo "$2:: I am learning Shell. What about you?"
