1433fc4ed59b80a2d93b1e4c310dabc7486914a9
Varun <varunkumar.pinnam@gmail.com>
2026-01-28 17:52:57 +0530

    updated varaiables templates

--- 4-special varaibles.sh
+++ 4-special varaibles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo " all args passed to script  : $@"
-echo " no of var passed to script : $#"
-echo "script name:                : $0"
-echo " present directory          : $PWD"
-echo "who is running the script   : $USER"
-echo "home directory of the user  : $HOME"
-echo " PID of recently BG process : $!"
-echo " Exit status of last command: $?"
+echo "all args passed to script      : $@"
+echo "no of var passed to script     : $#"
+echo "script name:                   : $0"
+echo "present directory              : $PWD"
+echo "who is running the script      : $USER"
+echo "home directory of the user     : $HOME"
+echo "PID of recently BG process     : $!"
+echo "Exit status of last command    : $?"
 
