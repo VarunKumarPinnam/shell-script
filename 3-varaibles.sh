f41df1572cc1fccbeb485ac741dcb59fbeb596bb
Varun <varunkumar.pinnam@gmail.com>
2026-01-28 17:30:17 +0530

    updated varaiables templates

--- 3-varaibles.sh
+++ 3-varaibles.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-START_TIME=$(Date +s%)
+START_TIME=$(date +s%)
 
 echo " start time is : $START_TIME"
 
 sleep 10
 
-END_TIME=$(Date +s%)
+END_TIME=$(date +s%)
 
 TOTAL_TIME=$($START_TIME-$END_TIME);
 
