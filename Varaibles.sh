80e6a3c4267da40d22aeb2ea440f00123ab233b8
Varun <varunkumar.pinnam@gmail.com>
2026-01-28 12:46:25 +0530

    commit

--- Varaibles.sh
+++ Varaibles.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 #P1=$1
-#2=$2
+#P2=$2
 
 echo "$1:: Hello $2, How are you?"
 echo "$2:: Hi $1, I am fine thanks for asking. How are you doing?"
 echo "$1:: doing fine. What are you learning $2?"
 echo "$2:: I am learning Shell. What about you?"
+
+## OR
+
+# PERSON1=Trump # no space between value and =
+# PERSON2=Putin
+
+# echo "$PERSON1:: Hello $PERSON2, How are you?"
+# echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+# echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
+# echo "$PERSON2:: I am learning Shell. What about you?"
