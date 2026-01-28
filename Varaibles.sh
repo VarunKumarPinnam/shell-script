07430909ce9b080402d3e29df48f73994737c145
Varun <varunkumar.pinnam@gmail.com>
2026-01-28 12:26:39 +0530

    commit

---
+++ Varaibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+P1=Putin
+P2=Trump
+
+echo "$P1:: Hello $P2, How are you?"
+echo "$P2:: Hi $P1, I am fine thanks for asking. How are you doing?"
+echo "$P1:: doing fine. What are you learning $P2?"
+echo "$P2:: I am learning Shell. What about you?"
