6944ca2f7a8298631772027db52974a08bac6ae8
Varun <varunkumar.pinnam@gmail.com>
2026-01-30 19:00:18 +0530

    updated varaiables templates

--- 7-installation.sh
+++ 7-installation.sh
@@ -2,17 +2,16 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ];then
-    echo "You should have root access to execute this script"
+if [ "$USERID" -ne 0 ]; then 
+    echo "Only root users can execute this script"
     exit 1
 fi
 
-echo  "Installing  Nginix"
-dnf install nginx -y
+echo "Installing ngnix"
+dnf install ngnix -y
 
-if [ $? -ne 0 ]; then
-    echo "Nginix installation failed"
-    exit 1
+if [ $? -ne 0 ]; then 
+    echo "Nginx installaion..failed"
 else
-    echo "Installation completed successfully"
+    echo "Ngnix installation..completed"
 fi
