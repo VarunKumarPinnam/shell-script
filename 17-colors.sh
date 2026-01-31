84289835450d5646179bc076cf331369a587705e
Varun <varunkumar.pinnam@gmail.com>
2026-01-31 14:36:14 +0530

    updated varaiables templates

--- 17-colors.sh
+++ 17-colors.sh
@@ -3,9 +3,14 @@
 USERID=$(id -u)
 LOGS_DIRECTORY="/var/log/shell-script"
 LOGS_FILE="$LOGS_DIRECTORY/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 
 if [ $USERID -ne 0 ]; then 
-    echo "You must run this script with root access" | tee -a $LOGS_FILE
+    echo "$R You must run this script with root access $N" | tee -a $LOGS_FILE
 fi
 
 mkdir -p $LOGS_DIRECTORY
@@ -13,11 +18,11 @@ mkdir -p $LOGS_DIRECTORY
 validation()
 {
   if [ $1 -eq 0 ]; then 
-    echo "$2 installation completed" | tee -a $LOGS_FILE
+    echo "$G $2 installation completed $N" | tee -a $LOGS_FILE
   elif [ $1 -eq 2 ];then
-    echo "$2 already installed..skipping the installation" | tee -a $LOGS_FILE
+    echo "$Y $2 already installed..skipping the installation $N" | tee -a $LOGS_FILE
   else
-    echo "$2 installation is failed" | tee -a $LOGS_FILE
+    echo "$R $2 installation is failed $N" | tee -a $LOGS_FILE
  fi
 }
 
@@ -26,13 +31,13 @@ install_if_missing()
   if dnf list installed $1 &>/dev/null; then
     return 2
 else
-    echo "Installing $1" | tee -a $LOGS_FILE
+    echo "$G Installing $1 $N" | tee -a $LOGS_FILE
     dnf install $1 -y &>> $LOGS_FILE
     return $?
 fi
 }
 
-for pkg in $@ # sudo dnf install nodejs nginx mysql 
+for pkg in $@ 
 do
     install_if_missing $pkg
     validation $? "$pkg"
