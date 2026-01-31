81997a3b02a5bcd4133e6c33fa334ec2597984f3
Varun <varunkumar.pinnam@gmail.com>
2026-01-31 14:48:52 +0530

    updated varaiables templates

--- 17-colors.sh
+++ 17-colors.sh
@@ -8,72 +8,40 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-# if [ "$USERID" -ne 0 ]; then 
-#     echo -e "$R You must run this script with root access $N" 
-# fi
-
-# mkdir -p $LOGS_DIRECTORY
-
-# validation()
-# {
-#   if [ $1 -eq 0 ]; then 
-#     echo -e "$G $2 installation completed $N" | tee -a $LOGS_FILE
-#   elif [ $1 -eq 2 ];then
-#     echo -e "$Y $2 already installed..skipping the installation $N" | tee -a $LOGS_FILE
-#   else
-#     echo -e "$R $2 installation is failed $N" | tee -a $LOGS_FILE
-#  fi
-# }
-
-# install_if_missing()
-# {
-#   if dnf list installed $1 &>/dev/null; then
-#     return 2
-# else
-#     echo  -e "$G Installing $1 $N" | tee -a $LOGS_FILE
-#     dnf install $1 -y &>> $LOGS_FILE
-#     return $?
-# fi
-# }
-
-# for pkg in $@ 
-# do
-#     install_if_missing $pkg
-#     validation $? "$pkg"
-# done
-
-# ---- ROOT CHECK ----
-if [ "$USERID" -ne 0 ]; then
-    echo -e "${R}You must run this script with root access${N}"
+#--ROOT User Check--
+if [ "$USERID" -ne 0 ]; then 
+    echo -e "$R You must run this script with root access $N" 
     exit 1
 fi
 
-# ---- LOG SETUP ----
-mkdir -p "$LOGS_DIRECTORY"
+#--Log Setup---
+mkdir -p $LOGS_DIRECTORY
 
-validation() {
-  if [ "$1" -eq 0 ]; then
-    echo -e "${G}$2 installation completed${N}" | tee -a "$LOGS_FILE"
-  elif [ "$1" -eq 2 ]; then
-    echo -e "${Y}$2 already installed..skipping the installation${N}" | tee -a "$LOGS_FILE"
+
+validation()
+{
+  if [ $1 -eq 0 ]; then 
+    echo -e "$G $2 installation completed $N" | tee -a $LOGS_FILE
+  elif [ $1 -eq 2 ];then
+    echo -e "$Y $2 already installed..skipping the installation $N" | tee -a $LOGS_FILE
   else
-    echo -e "${R}$2 installation is failed${N}" | tee -a "$LOGS_FILE"
-  fi
+    echo -e "$R $2 installation is failed $N" | tee -a $LOGS_FILE
+ fi
 }
 
-install_if_missing() {
-  if dnf list installed "$1" &>/dev/null; then
+install_if_missing()
+{
+  if dnf list installed $1 &>/dev/null; then
     return 2
-  else
-    echo -e "${G}Installing $1${N}" | tee -a "$LOGS_FILE"
-    dnf install "$1" -y &>> "$LOGS_FILE"
+else
+    echo  -e "$G Installing $1 $N" | tee -a $LOGS_FILE
+    dnf install $1 -y &>> $LOGS_FILE
     return $?
-  fi
+fi
 }
 
-# ---- PACKAGE LOOP ----
-for pkg in "$@"; do
-  install_if_missing "$pkg"
-  validation $? "$pkg"
+for pkg in $@ 
+do
+    install_if_missing $pkg
+    validation $? "$pkg"
 done
