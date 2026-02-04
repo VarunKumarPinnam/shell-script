0029bde24caf05d6a8e8ae1b5242d27734426454
Varun <varunkumar.pinnam@gmail.com>
2026-02-04 18:24:21 +0530

    updated varaiables templates

---
+++ 20-backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIRECTORY="/var/log/shell-script"
+SCRIPT_NAME=$(basename "$0" .sh)
+LOGS_FILE="$LOGS_DIRECTORY/${SCRIPT_NAME}_$(date '+%Y-%m-%d_%H-%M-%S').log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#--ROOT User Check--
+if [ "$USERID" -ne 0 ]; then 
+    echo -e "$R You must run this script with root access $N" 
+    exit 1
+fi
+
+mkdir -p $LOGS_DIRECTORY
+
+usage(){
+    echo -e "$R USAGE :: sudo backup <source_dir> <dest_dir> <days>[default 14 days] $N"
+    exit 1 
+}
+
+if [ $# -lt 2]; then
+    usage
+fi
+
