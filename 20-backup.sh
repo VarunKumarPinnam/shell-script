fd81c3723355d89a99fea5098a1db81817bae3f8
Varun <varunkumar.pinnam@gmail.com>
2026-02-04 19:15:50 +0530

    updated varaiables templates

--- 20-backup.sh
+++ 20-backup.sh
@@ -62,12 +62,12 @@ else
     TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE_NAME="$DEST_DIR/${DEST_DIR_NAME}-$TIMESTAMP.tar.gz"
     echo "Archieve name: $ZIP_FILE_NAME"
-    tar -zvcf $ZIP_FILE_NAME $FILES
+    tar -zvcf $ZIP_FILE_NAME $FILES 2>/dev/null
 
     #check archeival is success or not 
     if [ $? -eq 0 ]; then 
-        echo -e "$GArcheival is success$N"
+        echo -e "$G Archeival is success $N"
     else  
-        echo -e "$GArcheival is failed$N"
+        echo -e "$G Archeival is failed $N"
     fi
 fi
