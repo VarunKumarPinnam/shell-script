6472d29e6f13c1495033aa60d7fa879f23981f5e
Varun <varunkumar.pinnam@gmail.com>
2026-02-05 19:58:41 +0530

    updated varaiables templates

--- mail.sh
+++ mail.sh
@@ -6,7 +6,7 @@ MESSAGE_BODY=$3
 ALERT_TYPE=$4
 SERVER_IP=$5
 TO_TEAM=$6
-FINAL_MSG=$(echo $MSG | sed -e 's/[]\/$*.^[]/\\&/g')
+FINAL_MSG=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
 MSG=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MSG/g" template.html)
 
