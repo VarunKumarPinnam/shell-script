039090a0d871256fbfa304dd94f546c745f25b08
Varun <varunkumar.pinnam@gmail.com>
2026-02-05 17:44:50 +0530

    updated varaiables templates

--- 21-diskusage.sh
+++ 21-diskusage.sh
@@ -12,7 +12,7 @@ USAGE_THRESHOLD=3
 while IFS= read -r line
 do 
     USAGE=$(echo $line | awk '{print $6}' | cut -d '%' -f1)
-    PARTITION=$$(echo $line | awk '{print $7}')
+    PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -gt $USAGE_THRESHOLD ]; then
         MESSAGE+="High disk usage on $PARTITION: $USAGE"
     fi        
